952a5e7fa3ca63cff3b511667b8f679c04e3409c
bram.van.doren <bramvandoren1@hotmail.com>
2026-06-04 18:28:26 +0200

    updated config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -83,5 +84,4 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
